67942af2b1c077da932410fc9916178df4640bdf
SignpostMarv <github@signpostmarv.name>
2011-04-14 13:59:31 +0100

    More accurate but still a little buggy

--- canvasPolarTransformation.js
+++ canvasPolarTransformation.js
@@ -1,16 +1,44 @@
+
+
 (function(window,undefined){
 	var
 		document = window['document'],
 		xywh2polar = function(x,y,w,h){
 			return {'r':y/2.0,'t':((x - (w/2.0)) / (w/2.0)) * 180};
 		},
-		polarJS = function(canvas,resample){
+		polar2xy = function(r,t){
+			return {'x':r * Math.cos(t), 'y': r * Math.sin(t)};
+		},
+		xy2polar = function(x,y){
+			var
+				r = Math.sqrt(Math.pow(y,2) + Math.pow(x,2)),
+				t = 0
+			;
+			if(x > 0){
+				t = Math.atan(y / x);
+			}else if(x < 0 && y >= 0){
+				t = Math.atan(y/x) + Math.PI
+			}else if(x < 0 && y < 0){
+				t = Math.atan(y/x) - Math.PI;
+			}else if(x == 0 && y > 0){
+				t = Math.PI / 2;
+			}else if(x == 0 && y < 0){
+				t = 0 - (Math.PI / 2);
+			}
+			return {'r':r,'t':t};
+		},
+		xywh2polarxy = function(x,y,w,h){
+			return {'x':x - (w/2.0), 'y': y - (h/2.0)};
+		},
+		polar2txy = function(r,t,width,height){
+			
+		},
+		polarJS = function(canvas){
 			if(canvas == undefined){
 				throw 'Canvas element not defined';
 			}else if(!canvas.getContext){
 				throw 'canvas.getContext not supported';
 			}
-			resample = resample || 1;
 			var
 				ctx = canvas.getContext('2d')
 			;
@@ -18,89 +46,53 @@
 				throw new 'Could not get 2D context';
 			}
 			var
-				width = ctx.canvas.width,
-				height = ctx.canvas.height,
-				rscanvas = document.createElement('canvas'),
-				rsctx    = rscanvas.getContext('2d'),
-				acanvas = document.createElement('canvas'),
-				actx    = acanvas.getContext('2d')
+				width    = ctx.canvas.width,
+				height   = ctx.canvas.height,
+				scaled   = document.createElement('canvas'),
+				sctx     = scaled.getContext('2d'),
+				polar    = document.createElement('canvas'),
+				pctx     = polar.getContext('2d')
 			;
-			rscanvas.width = width * resample;
-			rscanvas.height = height * resample;
-			rsctx.scale(resample,resample);
-			rsctx.drawImage(canvas,0,0);
-			width = rscanvas.width;
-			height = rscanvas.height;
 
-			acanvas.width  = width + (1 - (width % 2));
-			acanvas.height = height + (1 - (width % 2));
-			if(acanvas.width > acanvas.height){
-				acanvas.height = acanvas.width;
-			}else if(acanvas.width < acanvas.height){
-				acanvas.width = acanvas.height;
+			scaled.width  = width;
+			scaled.height = height;
+			if(height > width){
+				scaled.width = height;
+			}else if(width > height){
+				scaled.height = width;
 			}
+			sctx.scale(scaled.width / width, scaled.height / height);
+			sctx.drawImage(canvas,0,0);
 
-			actx.scale(acanvas.width / width,acanvas.height / height);
-			actx.drawImage(rscanvas,0,0);
-
+			polar.width = polar.height = scaled.width;
 			var
-				adata   = actx.getImageData(0,0,acanvas.width, acanvas.height),
-				bcanvas = document.createElement('canvas'),
-				bctx    = bcanvas.getContext('2d')
+				radius = polar.width / 2.0,
+				pdata  = pctx.getImageData(0,0,polar.width,polar.height)
 			;
-			bcanvas.width = bcanvas.height = acanvas.width;
-			var
-				bdata   = bctx.createImageData(bcanvas.width, bcanvas.height),
-				avgs    = {}
-			;
-
-			for(var y=0; y < adata.height; ++y){
-				for(var x=0;x<adata.width;++x){
+			for(var y=0;y<polar.height;++y){
+				for(var x=0;x<polar.width;++x){
 					var
-						rt = xywh2polar(x,y,adata.width,adata.height),
-						rtx = rt.r * Math.cos(rt.t),
-						rty = rt.r * Math.sin(rt.t),
-						bx = Math.floor((bcanvas.width / 2.0) + rtx),
-						by = Math.floor((bcanvas.height / 2.0) + rty),
-						ai = (x  * 4) + (y  * 4) * acanvas.width,
-						bi = (bx * 4) + (by * 4) * bcanvas.width
+						i  = (x * 4) + (y * 4) * polar.width,
+						xy = xywh2polarxy(x,y,polar.width,polar.height),
+						rt = xy2polar(xy.x,xy.y)
 					;
-					if(avgs[bi] == undefined){
-						avgs[bi] = [];
+					if(rt.r > radius){
+						continue;
 					}
-					if(avgs[bi + 1] == undefined){
-						avgs[bi + 1] = [];
-					}
-					if(avgs[bi + 2] == undefined){
-						avgs[bi + 2] = [];
-					}
-					if(avgs[bi + 3] == undefined){
-						avgs[bi + 3] = [];
-					}
-					avgs[bi + 0].push(adata.data[ai + 0]);
-					avgs[bi + 1].push(adata.data[ai + 1]);
-					avgs[bi + 2].push(adata.data[ai + 2]);
-					avgs[bi + 3].push(adata.data[ai + 3]);
+					var
+						sourceX    = Math.floor(radius + ((rt.t * (180/Math.PI)) / 180)),
+						sourceY    = Math.max(0,Math.floor(((rt.r / radius) * scaled.height)) - 1),
+						sourceData = sctx.getImageData(sourceX,sourceY,1,1)
+					;
+					pdata.data[i + 0] = sourceData.data[0];
+					pdata.data[i + 1] = sourceData.data[1];
+					pdata.data[i + 2] = sourceData.data[2];
+					pdata.data[i + 3] = sourceData.data[3];
 				}
 			}
-			for(var i in avgs){
-				var sum = 0;
-				for(var j=0;j<avgs[i].length;++j){
-					sum += avgs[i][j];
-				}
-				bdata.data[i] = sum / avgs[i].length;
-			}
-			bctx.putImageData(bdata,0,0);
+			pctx.putImageData(pdata,0,0);
 
-			var
-				ccanvas = document.createElement('canvas'),
-				cctx    = ccanvas.getContext('2d')
-			;
-			ccanvas.width = canvas.width;
-			ccanvas.height = canvas.height;
-			cctx.scale(ccanvas.width / bcanvas.width, ccanvas.height / bcanvas.height);
-			cctx.drawImage(bcanvas,0,0);
-			return ccanvas;
+			return polar;
 		}
 	;
 
